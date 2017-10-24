7653af89686b6b6653167a3c5bcfe8c60d4789c3
Levi Donaldson <lcdbacanas@gmail.com>
2017-10-23 19:59:26 -0500

    Update assocArray.php

--- assocArray.php
+++ assocArray.php
@@ -1,70 +1,74 @@
 <?php
 
- $allColors = array(
-  '0' => array(
-  "id"=> "1",
-  "family"=> "blue",
-  "name"=> "ariel blue",
-  "hex"=> "#339FFF"),
+$allColors = array(
+ '0' => array(
+ "id"=> "1",
+ "family"=> "blue",
+ "name"=> "ariel blue",
+ "hex"=> "#339FFF "),
 
-  '1' => array(
-  "id"=> "2",
-  "family"=> "yellow",
-  "name"=> "canary",
-  "hex"=> "#FAFF33"),
+'3' => array(
+ "id"=> "3",
+ "family"=> "green",
+ "name"=> "forest",
+ "hex"=> "#FAFF33 "),
 
-  '2' => array(
-    "id"=> "3",
-    "family"=> "blue",
-    "name"=> "sky blue",
-    "hex"=> "#33C7FF"),
-    
-  '2' => array(
-    "id"=> "3",
-    "family"=> "blue",
-    "name"=> "sky blue",
-    "hex"=> "#33C7FF"),  
+'1' => array(
+ "id"=> "2",
+ "family"=> "blue",
+ "name"=> "aqua marine",
+ "hex"=> "#339FFF "),
 
+'4' => array(
+ "id"=> "4",
+ "family"=> "green",
+ "name"=> "lime",
+ "hex"=> "#FAFF33 "),
+
+'2' => array(
+ "id"=> "5",
+ "family"=> "blue",
+ "name"=> "teal",
+ "hex"=> "#339FFF "),
+
+'5' => array(
+ "id"=> "6",
+ "family"=> "green",
+ "name"=> "yellow-green",
+ "hex"=> "#FAFF33 ")
 ); 
 
-// print_r($allColors);
+array_multisort( array_column( $allColors, 'family') , SORT_DESC, $allColors );
+
+$arGroupedByFamily = [];
+foreach ($allColors as $color) {
+   $arGroupedByFamily[$color['family']][] = $color;
+}
+
 ?> 
+
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <style>
-  p { margin: 0; }
-  </style>
- 
-  <body> 
+   <head>
+       <meta charset="utf-8">
+       <title></title>
+       <link rel="stylesheet" type="text/css" href="main.css">
+   </head>
 
-  <table>
-   <?php foreach($allColors as $key => $value): ?>
-   <tr>
-       <td><?php //echo '<p>'.$value["family"]. '</p>'; ?></td>
-   </tr>
-   <?php endforeach; ?>
-  </table>
-
-  
-  <div id="blueFamily" style="border: 2px solid blue; padding: 0.5rem;">
-    
-    <div id="familyTitle">
-      <p style="padding-left: 1rem;font-weight: bold;"><?php echo $value["family"]?></p> 
-    </div>
-
-    <div id="familyColors" style="min-height: 300px;padding:0.5rem;">
-      <div class="mediaObject" style="padding: 0.2rem;margin-top: 1rem;height: 100px;width: 80px;border: 1px solid #f6f6f6;">
-        <div style="background-color: <?php echo $value["hex"]?>;height: 70px;width: 70px;padding:0;margin: 0 auto;"></div>
-        <p style="font-size: 12px;text-align:center;"><?php echo $value["name"]?></p>
-        <p style="font-size: 10px;text-align:center;"><?php echo $value["hex"]?></p>
-      </div>
-    </div>
-
-  </div>
-    
-  </body>
+   <body>
+       <?php foreach ($arGroupedByFamily as $key => $family): ?>
+           <div id="family-container" style="border: 1px solid <?php echo $key; ?>; background-color: $currFamily;">
+               <?php foreach ($family as $color): ?>
+                   <h2><?php echo $color['name'];?> <?php echo $key; ?></h2>
+                   <div class="child-container" style="margin-right: 20px; width: 100px; height: 150px; border: 1px solid black; float: left;">
+                       <div style="width: 100%; height: 100px; float: right; border-bottom: 1px solid black; background: <?php echo $color["hex"]; ?>;"></div>
+                       <div style="width: 100%; height: 50px; float: right;">
+                           <?php echo $color["name"]; ?>;
+                           <?php echo $color["hex"]; ?>;
+                       </div>
+                   </div>
+               <?php endforeach; ?>
+           </div>
+       <?php endforeach; ?>
+   </body>
 </html>
