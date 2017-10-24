e860b52762d7c6c1e521f43cf7001b7ca080a9f4
Levi Donaldson <lcdbacanas@gmail.com>
2017-10-24 14:51:21 -0500

    Update assocArray.php

--- assocArray.php
+++ assocArray.php
@@ -3,48 +3,47 @@
 $allColors = array(
  '0' => array(
  "id"=> "1",
- "family"=> "blue",
- "name"=> "ariel blue",
- "hex"=> "#339FFF "),
+ "family"=> "Blue",
+ "name"=> "Sky Blue",
+ "hex"=> "#89ccff "),
 
 '3' => array(
  "id"=> "3",
- "family"=> "green",
- "name"=> "forest",
- "hex"=> "#FAFF33 "),
+ "family"=> "Green",
+ "name"=> "Emerald",
+ "hex"=> "#018910 "),
 
 '1' => array(
  "id"=> "2",
- "family"=> "blue",
- "name"=> "aqua marine",
- "hex"=> "#339FFF "),
+ "family"=> "Blue",
+ "name"=> "Royal Blue",
+ "hex"=> "#2f62dc "),
 
 '4' => array(
  "id"=> "4",
- "family"=> "green",
- "name"=> "lime",
- "hex"=> "#FAFF33 "),
+ "family"=> "Green",
+ "name"=> "Lime",
+ "hex"=> "#aef35a "),
 
 '2' => array(
  "id"=> "5",
- "family"=> "blue",
- "name"=> "teal",
- "hex"=> "#339FFF "),
+ "family"=> "Blue",
+ "name"=> "Turquoise",
+ "hex"=> "#00ffef "),
 
 '5' => array(
  "id"=> "6",
- "family"=> "green",
- "name"=> "yellow-green",
- "hex"=> "#FAFF33 ")
+ "family"=> "Green",
+ "name"=> "Sea Foam",
+ "hex"=> "#3cec97")
 ); 
 
-array_multisort( array_column( $allColors, 'family') , SORT_DESC, $allColors );
+array_multisort( array_column( $allColors, 'family'), SORT_DESC, $allColors );
 
 $arGroupedByFamily = [];
 foreach ($allColors as $color) {
    $arGroupedByFamily[$color['family']][] = $color;
 }
-
 ?> 
 
 <!DOCTYPE html>
@@ -57,18 +56,20 @@ foreach ($allColors as $color) {
 
    <body>
        <?php foreach ($arGroupedByFamily as $key => $family): ?>
-           <div id="family-container" style="border: 1px solid <?php echo $key; ?>; background-color: $currFamily;">
+           <div id="family-container" style="border: 2px solid <?php echo $key; ?>; background-color: $currFamily; width: 100%; display: inline-block; margin-bottom: 1rem; padding: 0.5rem;">
+               <h2><?php echo $key; ?></h2>
                <?php foreach ($family as $color): ?>
-                   <h2><?php echo $color['name'];?> <?php echo $key; ?></h2>
                    <div class="child-container" style="margin-right: 20px; width: 100px; height: 150px; border: 1px solid black; float: left;">
                        <div style="width: 100%; height: 100px; float: right; border-bottom: 1px solid black; background: <?php echo $color["hex"]; ?>;"></div>
-                       <div style="width: 100%; height: 50px; float: right;">
-                           <?php echo $color["name"]; ?>;
-                           <?php echo $color["hex"]; ?>;
+                       <div style="width: 100%; height: 50px; float: right; padding-top: 5px; text-align: center;">
+                           <?php echo $color["name"]; ?> 
+                           <br>
+                           <?php echo $color["hex"]; ?>
                        </div>
                    </div>
                <?php endforeach; ?>
            </div>
+           
        <?php endforeach; ?>
    </body>
 </html>
